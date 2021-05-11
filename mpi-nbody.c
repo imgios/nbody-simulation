8a7d5c397da2c870a1480620e0a8356547572305
Giosuè Sulipano <sulipano.gs@gmail.com>
2021-05-11 17:05:17 +0200

    Add a review comment

--- mpi-nbody.c
+++ mpi-nbody.c
@@ -59,7 +59,7 @@ int main (int argc, int ** argv) {
 
     // Init MPI env
     MPI_Init(&argc, &argv);
-    MPI_Type_contiguous( 6, MPI_FLOAT, &bodyType);
+    MPI_Type_contiguous( 6, MPI_FLOAT, &bodyType); // REVIEW: Contiguous or Struct?
     MPI_Type_commit(&bodyType);
     MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
