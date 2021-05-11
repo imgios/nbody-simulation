6f136a0f0d7a5c073d7d84944e483e1f1234d2c0
Giosuè Sulipano <sulipano.gs@gmail.com>
2021-05-11 16:18:47 +0200

    Define Struct for Bodies

--- mpi-nbody.c
+++ mpi-nbody.c
@@ -1,6 +1,14 @@
 #include "mpi.h"
 #include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include "timer.h"
 
+// Struct for Bodies
+typedef struct {
+    float x, y, z; // position
+    float vx, vy, vz; // velocity
+} Body;
 int main (int argc, int ** argv) {
     int numtasks, rank, tag = 1;
     // TODO: Buffer declaration
